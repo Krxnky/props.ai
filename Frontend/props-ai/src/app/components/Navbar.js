8d73cf725d3b118d17bc67c38bdf70d48908fa9b
cjwaavy <33335818+cjwaavy@users.noreply.github.com>
2024-03-26 13:06:02 +0000

    latest

--- Frontend/props-ai/src/app/components/Navbar.js
+++ Frontend/props-ai/src/app/components/Navbar.js
@@ -1,11 +1,26 @@
+use client
 import Image from "next/image";
+import { Dropdown } from '@nextui-org/react';
 
 export default function Navbar() {
     return (
+        
         <div className='flex min-w-screen bg-zinc-800 drop-shadow-xl justify-center'>
+            
             <Image src={"/props_ai_temp_logo.png"} width={50} height={50}>
 
             </Image>
+            <Dropdown>
+                <Dropdown.Button flat>Trigger</Dropdown.Button>
+                <Dropdown.Menu aria-label="Static Actions">
+                    <Dropdown.Item key="new">New file</Dropdown.Item>
+                    <Dropdown.Item key="copy">Copy link</Dropdown.Item>
+                    <Dropdown.Item key="edit">Edit file</Dropdown.Item>
+                    <Dropdown.Item key="delete" color="error">
+                    Delete file
+                    </Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
         </div>
     )
 }
