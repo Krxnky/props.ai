1d5707457e7b73321b89f0a0968207078810fc6b
Krxnky <rylanelarson@gmail.com>
2024-03-27 08:51:54 -0500

    latest

---
+++ Backend/model.py
@@ -0,0 +1,69 @@
+import pandas as pd
+from datetime import date
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from nba_api.stats.endpoints import PlayerGameLog, ScoreboardV2, CommonTeamRoster
+from nba_api.stats.static import players
+
+class PropsModelV1():
+    def __init__(self, player_name, stat_type, line_score):
+        self.player_name = player_name
+        self.stat_type = stat_type
+        self.line_score = line_score
+        print(self.player_name, self.stat_type, self.line_score)
+
+    def get_prediction(self):
+        player_id = players.find_players_by_full_name(self.player_name)[0]["id"]
+        point_threshold = self.line_score
+
+        game_log = PlayerGameLog(player_id=player_id, season="2023-24").get_data_frames()[0]
+        game_log2 = PlayerGameLog(player_id=player_id, season="2022-23").get_data_frames()[0]
+        print('GOT GAME LOG')
+        column = game_log[['Game_ID', 'GAME_DATE', 'MATCHUP', 'PTS', 'AST', 'STL', 'REB', 'TOV','FGA','FGM', 'FG3M', 'FG3A', 'BLK','FTA','FTM','PLUS_MINUS']]
+        column2 = game_log2[['Game_ID', 'GAME_DATE', 'MATCHUP', 'PTS', 'AST', 'STL', 'REB', 'TOV','FGA','FGM', 'FG3M', 'FG3A', 'BLK','FTA','FTM','PLUS_MINUS']]
+        frames = [pd.DataFrame(column), pd.DataFrame(column2)]
+        df = pd.concat(frames)
+
+        df['PTS+REB+AST'] = df['PTS'] + df['REB'] + df['AST']
+        df['PTS+AST'] = df['PTS'] + df['AST']
+        df['REB+AST'] = df['REB'] + df['AST']
+        df['PTS+REB'] = df['PTS'] + df['REB']
+        df['BLKS+STLS'] = df['BLK'] + df['STL']
+
+        df['HOME'] = df['MATCHUP'].apply(lambda x: 1 if 'vs. ' in x else 0)
+        df['OPPONENT'] = df['MATCHUP'].apply(lambda x: x.split()[-1])
+
+        features = ['HOME', 'AST', 'STL', 'REB', 'TOV', 'FG3M', 'FG3A', 'BLK', 'FGA', 'FGM', 'FTA', 'FTM', 'PLUS_MINUS']
+        target = 'PTS'
+
+        x = df[features]
+        y = df[target]
+
+        y = y.apply(lambda x: 1 if x > point_threshold else 0)
+
+        scaler = StandardScaler()
+        x_scaled = scaler.fit_transform(x)
+
+        x_train, x_test, y_train, y_test = train_test_split(x_scaled, y, test_size=0.4, random_state=42)
+
+        logisitc_regression_model = LogisticRegression(max_iter=1000)
+        logisitc_regression_model.fit(x_train, y_train)
+
+        predictions = logisitc_regression_model.predict(x_test)
+
+        accuracy = accuracy_score(y_test, predictions)
+        precision = precision_score(y_test, predictions)
+        recall = recall_score(y_test, predictions)
+        f1 = f1_score(y_test, predictions)
+
+        return {
+            'accuracy': accuracy,
+            'precision': precision,
+            'recall': recall,
+            'f1': f1,
+            'prediction': 'OVER' if predictions[0] == 1 else 'UNDER'
+        }
--- Backend/server.py
+++ Backend/server.py
@@ -1,8 +1,10 @@
 import time
+import json
 from datetime import date
 from flask import Flask, request
 from nba_api.stats.endpoints import playergamelog, ScoreboardV2, CommonTeamRoster
 from nba_api.stats.static.teams import find_team_name_by_id
+from model import PropsModelV1
 
 app = Flask(__name__)
 
@@ -47,4 +49,50 @@ def get_players():
 
 @app.route('/get-props')
 def get_props():
-    return ''
+    # TODO: FETCH PROPS FROM PRIZEPICKS
+    with open('./projections.json', 'r') as file:
+        json_data = json.load(file)
+        seive = {"points", "rebounds", "assists", "threes", "blocks", "steals", "pra", "pr", "pa", "ra"}
+        player_names = {elem["id"]: elem["attributes"]["name"]
+                        for elem in json_data["included"]
+                        if elem["type"] == "new_player"}
+        player_projections = []
+        for projection in json_data["data"]:
+            if projection["type"] == "projection":
+                player_id = projection["relationships"]["new_player"]["data"]["id"]
+                player_name = player_names.get(player_id, "Unknown Player")
+
+                flash_sale = projection["attributes"].get("flash_sale_line_score")
+                line_score = projection["attributes"]["line_score"]
+                stat_type = projection["attributes"]["stat_type"]
+                start_time = projection["attributes"]["start_time"]
+
+                if stat_type == "Pts+Rebs+Asts":
+                    stat_type = "pra"
+                if stat_type == "Pts+Asts":
+                    stat_type = "pa"
+                if stat_type == "Pts+Rebs":
+                    stat_type = "pr"
+                if stat_type == "Rebs+Asts":
+                    stat_type = "ra"
+                if stat_type == "3-PT Made":
+                    stat_type = "threes"
+                if stat_type == "Blocked Shots":
+                    stat_type = "blocks"
+
+                if stat_type in seive:
+                    player_projections.append({
+                        'player_name': player_name,
+                        'player_id': player_id,
+                        'stat_type': stat_type,
+                        'line_score': line_score,
+                        'start_time': start_time
+                    })
+        data = []
+        for player in player_projections:
+            model = PropsModelV1(player['player_name'], player['stat_type'], player['line_score'])
+            prediction = model.get_prediction()
+            data.append(player.update(prediction))
+                
+
+        return data
