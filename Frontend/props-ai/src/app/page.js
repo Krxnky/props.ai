614081edb84448bab0afb9f2a8a25e313d26161f
cjwaavy <33335818+cjwaavy@users.noreply.github.com>
2024-04-01 13:06:41 +0000

    frtonend latest

--- Frontend/props-ai/src/app/page.js
+++ Frontend/props-ai/src/app/page.js
@@ -1,9 +1,21 @@
 import Image from "next/image";
 import Link from "next/link";
 import PropItem from "./dashboard/PropItem";
+// import getStaticProps from "@/requests/getStaticProps";
 
 export default function Home() {
-  
+  async function getStaticProps() {
+    const response = await fetch('http://localhost:5000/props?platform=prizepicks');
+    const data = await response.json();
+
+    return {
+        props: {
+            jsonData: data
+        }
+    };
+}
+  const data = getStaticProps()
+  console.log(data)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-zinc-900">
       <Link className="text-white" href={"/dashboard"}>Dashboard Link</Link>
