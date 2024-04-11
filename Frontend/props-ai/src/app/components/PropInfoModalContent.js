b48c3ae888df445b2dc69a03a267005319333972
cjwaavy <33335818+cjwaavy@users.noreply.github.com>
2024-04-11 13:05:20 +0000

    latest

--- Frontend/props-ai/src/app/components/PropInfoModalContent.js
+++ Frontend/props-ai/src/app/components/PropInfoModalContent.js
@@ -1,24 +1,28 @@
 import { Button } from "@nextui-org/react"
 import Link from "next/link"
+import Image from "next/image";
 import { useDispatch, useSelector } from "react-redux"
 
 export default function PropInfoModalContent(){
     const activePlayer = useSelector((state) => state.activePlayer.value)
     const dispatch = useDispatch()
     return(
-        <div className="flex ease-out duration-300">
+        <div>
                     <dialog
                         className="fixed left-0 top-0 w-full h-full bg-black bg-opacity-10 z-50 overflow-auto backdrop-blur-sm flex justify-center items-center">
                         <div className="bg-zinc-900 m-auto p-8 rounded-md">
-                            <div className="flex flex-row">
-                                <h3 className="text-white">{activePlayer.player_name}</h3>
-                                <br/>
+                            <div className="flex flex-col">
                                 <Button
                                     onClick={() => dispatch({type: 'propItemModal/changePropItemModal', payload: false})}
-                                    className="bg-red-500 text-white p-2 "
                                 >
-                                    Close Modal
+                                    <Image 
+                                        src={`/close.svg`}
+                                        width={25}
+                                        height={25}
+                                        />
                                 </Button>
+                                <h3 className="text-white">{activePlayer.player_name}</h3>
+                                <br/>
                             </div>
 
                         </div>
