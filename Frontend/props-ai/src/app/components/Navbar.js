67f158234c511ad7d50a07b1647eb8b83a73c47e
cjwaavy <33335818+cjwaavy@users.noreply.github.com>
2024-04-23 13:54:04 +0000

    latest

--- Frontend/props-ai/src/app/components/Navbar.js
+++ Frontend/props-ai/src/app/components/Navbar.js
@@ -1,18 +1,15 @@
 import Image from "next/image";
 import Dropdown from "./Dropdown";
-import {Roboto } from "@next/font"
 import { Inconsolata } from "next/font/google";
-export default function Navbar() {
-    const roboto = Inconsolata({
-        subsets: ['latin'],
-        weight: ['200','300','400']
-    })
-    return (
-        <div className='flex min-w-screen bg-zinc-800 drop-shadow-xl justify-around'>
-            
-            <Image src={"/props_ai_temp_logo.png"} width={50} height={50}>
 
-            </Image>
+const inconsolata = Inconsolata({
+    subsets: ['latin'],
+    weight: ['300']
+})
+export default function Navbar() {
+    return (
+        <div className='flex min-w-screen bg-zinc-800 drop-shadow-xl justify-around text-white text-4xl py-4'>
+            <h1 className={inconsolata.className}>PROPS.AI</h1>
         </div>
     )
 }
