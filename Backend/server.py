9772cac8788f32ea3b6ef90bc8694528a9b22dfd
Charles McClasky <111356@tisd-ad.tomballisd.net>
2024-05-14 14:34:29 -0500

    latest afer school

--- Backend/server.py
+++ Backend/server.py
@@ -1,5 +1,6 @@
 import time
 import json
+import pandas
 from datetime import date
 from flask import Flask, request
 from nba_api.stats.endpoints import PlayerGameLog, ScoreboardV2, CommonTeamRoster
@@ -90,8 +91,50 @@ def get_props():
 
 @app.route('/last10')
 def get_last_ten():
+    def convert_date_format(date_str):
+        try:
+            date_obj = pandas.to_datetime(date_str, format='%b %d, %Y')
+            return date_obj.strftime('%m-%d-%Y')
+        except ValueError:
+            return None
+
     player_id = request.args.get('player_id')
     stat_type = request.args.get('stat_type')
-    pgl = PlayerGameLog(player_id=player_id).get_data_frames()[0][0:10]
+    PlayoffGameLog = PlayerGameLog(player_id=player_id, season="2023-24", season_type_all_star="Playoffs").get_data_frames()[0]
+    PlayoffGameLog["SEASON_TYPE"] = "Playoffs"
+
+    if(len(PlayoffGameLog) >= 10):
+        PlayoffGameLog = PlayoffGameLog[0:10]
+        
+    RegularSeasonGameLog = PlayerGameLog(player_id=player_id).get_data_frames()[0] if len(PlayoffGameLog) < 10 else None
+    df = None
+    if(RegularSeasonGameLog is not None):
+        RegularSeasonGameLog["SEASON_TYPE"] = "Regular Season"
+        df = pandas.concat([RegularSeasonGameLog[[stat_type, "MATCHUP", "GAME_DATE", "SEASON_TYPE"]], PlayoffGameLog[[stat_type, "MATCHUP", "GAME_DATE", "SEASON_TYPE"]]][0:(10 - len(PlayoffGameLog))])
+    else:
+        df = PlayoffGameLog[stat_type, "MATCHUP", "GAME_DATE", "SEASON_TYPE"]
     returnable = []
-    return type(pgl)
+
+    df = df.sort_values(by="GAME_DATE", ascending=False)
+    
+    for index, row in df.iterrows():
+        returnable.append({
+            "isHome": "false" if "@" in row.iloc[1] else "true",
+            "stat_score": row.iloc[0],
+            "vs": row.iloc[1].split(' ')[2],
+            "date": convert_date_format(row.iloc[2]),
+            "season_type": row.iloc[3]
+        })
+    return returnable
+    # stat_type = "PTS"
+    # pgl = PlayerGameLog(player_id=203999, season="2023-24", season_type_all_star="Playoffs").get_data_frames()[0][0:10]
+    # df = pgl[[stat_type, "MATCHUP", "GAME_DATE"]]
+    # returnable = []
+    # for index, row in df.iterrows():
+    #     returnable.append({
+    #         "isHome": "false" if "@" in row.iloc[1] else "true",
+    #         "stat_score": row.iloc[0],
+    #         "vs": row.iloc[1].split(' ')[2],
+    #         "date": row.iloc[2]    
+    #     })
+    # return returnable
